fcf36dad56b79d985990e5ff1481303433ea92c2
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2021-12-13 16:26:47 +0900

    사용자 삭제 기능 완성. 관리자-사용자관리 모두 완성

--- project/src/controllers/admin/user/UserController.js
+++ project/src/controllers/admin/user/UserController.js
@@ -8,6 +8,7 @@ router.get("/", async (req, res) => {
   let { searchOption, searchKeyword } = req.query;
   let pageNo;
   let message = "";
+  let errorMessage = "";
 
   // 현재페이지가 없으면 1로 지정
   if (req.query.pageNo) pageNo = parseInt(req.query.pageNo);
@@ -29,10 +30,21 @@ router.get("/", async (req, res) => {
   let minContent = (pageNo - 1) * 10;
   let maxContent = minContent + 10;
 
+  // 클라이언트에 보여줄 메시지가 있으면 처리
+  if (req.session.message) {
+    message = req.session.message;
+    req.session.message = null;
+  }
+  if (req.session.errorMessage) {
+    errorMessage = req.session.errorMessage;
+    req.session.errorMessage = null;
+  }
+
   res.render("admin/main.ejs", {
     pageName: "userList",
     sectionName: "user",
     message, // 화면에 보여줄 메시지
+    errorMessage,
     userList: userList.slice(minContent, maxContent), // 보여줄 음식 목록
     numOfListItem: numOfUser, // 보여줄 음식 목록 갯수
     pageNo, // 보고 있는 페이지 번호
@@ -73,6 +85,21 @@ router.get("/exercise", async (req, res) => {
     exerciseList,
     message,
   });
+});
+
+// 사용자 삭제 처리
+router.post("/removal/:username", async (req, res) => {
+  let { username } = req.params;
+  let user = await User.findOne({username});
+
+  if (user) {
+    await User.findOneAndRemove({username});
+    req.session.message = `사용자 ${username} 삭제 완료!`;
+  } else {
+    req.session.errorMessage = `사용자 ${username} 는 없습니다.`;
+  }
+
+  res.redirect("/admin/user");
 });
 
 module.exports = router;
